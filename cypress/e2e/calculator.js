924cfa8002976a6aaf2856ed14c0a3eaa91deb27
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-04-01 21:43:01 +0200

    testing-library/cypress setup for test simplification

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -1,13 +1,10 @@
 describe('anonymous calculator', () => {
   it('can make calculations', () => {
     cy.visit('/')
-    cy.get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(3)').click()
-    cy.get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(4)').click()
-    cy.get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(4)').click()
-    cy.get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(5)').click()
-    cy.get('.css-1bzsv8h-calculator-display--DisplayContainer').should(
-      'have.text',
-      '3',
-    )
+    cy.findByText(/^1$/).click()
+    cy.findByText(/^\+$/).click()
+    cy.findByText(/^2$/).click()
+    cy.findByText(/^=$/).click()
+    cy.findByTestId('total').should('have.text', '3')
   })
 })
