4ce1f28eb75b63cae460528eb130cc88cb48d58a
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-04-01 21:58:19 +0200

    debuger examples

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -3,8 +3,15 @@ describe('anonymous calculator', () => {
     cy.visit('/')
     cy.findByText(/^1$/).click()
     cy.findByText(/^\+$/).click()
-    cy.findByText(/^2$/).click()
+    cy.findByText(/^2$/)
+      // .then(subject => {
+      //   debugger
+      //   return subject
+      // })
+      // .debug()
+      .click()
     cy.findByText(/^=$/).click()
+
     cy.findByTestId('total').should('have.text', '3')
   })
 })
