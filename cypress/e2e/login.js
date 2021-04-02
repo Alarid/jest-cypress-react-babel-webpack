c1c79a1e4408d3ffe8e0d95ed05799d769ffc63f
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-04-02 14:05:39 +0200

    add custom cypress command to register new users

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -1,25 +1,18 @@
-import {buildUser} from '../support/generate'
-
 describe('login', () => {
   it('should login an existing user', () => {
     // create a user
-    const user = buildUser()
-    cy.request({
-      url: 'http://localhost:3000/register',
-      method: 'POST',
-      body: user,
+    cy.createUser().then(user => {
+      // now our test can start
+      cy.visit('/')
+      cy.findByText(/login/i).click()
+      cy.findByLabelText(/username/i).type(user.username)
+      cy.findByLabelText(/password/i).type(user.password)
+      cy.findByText(/submit/i).click()
+
+      cy.window()
+        .its('localStorage.token')
+        .should('be.a', 'string')
+      cy.findByTestId('username-display').should('have.text', user.username)
     })
-
-    // now our test can start
-    cy.visit('/')
-    cy.findByText(/login/i).click()
-    cy.findByLabelText(/username/i).type(user.username)
-    cy.findByLabelText(/password/i).type(user.password)
-    cy.findByText(/submit/i).click()
-
-    cy.window()
-      .its('localStorage.token')
-      .should('be.a', 'string')
-    cy.findByTestId('username-display').should('have.text', user.username)
   })
 })
